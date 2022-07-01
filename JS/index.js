eb519c5e216b525a7d7eb03d4db19b9e47beaa0f
CharlieCharlie79 <carloswork067@hotmail.com>
2022-07-01 11:52:49 -0400

    Navbar_toggle_added_not_finished

--- JS/index.js
+++ JS/index.js
@@ -1,25 +1,27 @@
-/*
-const hamburgerBtn = document.getElementById('hamburger-id');
 
-var navUl = document.getElementById('nav-menu');
+const menu = document.querySelector(".nav-menu");
+const menuItems = document.querySelectorAll(".nav-link");
+const hamburger = document.querySelector(".hamburger");
+const closeIcon = document.querySelector(".close-icon");
+const menuIcon = document.querySelector(".menuIcon");
 
-hamburgerBtn.addEventListener("click", zeroLeft());
-
-function zeroLeft()
-{
-     navUl.classList.add("cero-left");
-     console.log("clicking");
-}
-*/
-
-var hamburgerBtn = document.getElementById('hamburger-id');
-
-function showNav()
-{
-     document.getElementById("nav-ul").classList.toggle('cero-left');
-     console.log("clicking");
+function toggleMenu() {
+     if(menu.classList.contains("showNav")){
+          menu.classList.remove("showNav");
+          closeIcon.style.display = "none";
+          menuIcon.style.display = "block";
+     }
+     else{
+          menu.classList.add("showNav");
+          closeIcon.style.display = "block";
+          menuIcon.style.display = "none";
+     }
 }
 
-hamburgerBtn.addEventListener("click", showNav());
+hamburger.addEventListener("click", toggleMenu);
 
-
+menuItems.forEach(
+     function(menuItem){
+          menuItem.addEventListener("click", toggleMenu);
+     }
+)
